78e6ba58cd33c91c25da6bf6d495d25d34c0ad71
kparco <parcokaren@gmail.com>
2019-07-27 21:38:24 +0800

    added firebase

--- js/app.js
+++ js/app.js
@@ -1,33 +1,37 @@
-const entryTitle = document.getElementById('book-title');
+var titleBook = document.getElementById('book-title');
+// var author = document.getElementById('book-author');
+// var pages = document.getElementById('book-pages');
+// var status = document.getElementById('book-status');
 
-const btnShowForm = document.getElementById('btnAdd');
-const btnCloseForm = document.getElementById('cancelButton');
-const newBookForm = document.getElementById('entryForm');
-const formContainer = document.querySelector('.addForm-container');
-const main = document.querySelector('.container');
+var btnShowForm = document.getElementById('btnAdd');
+var btnCloseForm = document.getElementById('cancelButton');
+var btnSubmit = document.getElementById('submit');
+var newBookForm = document.getElementById('entryForm');
 
-const openForm = e => {
-    newBookForm.style.visibility = 'visible';
-    main.classList.add('blur');
-}
 
-const closeForm = e => {
-    newBookForm.style.visibility = 'hidden';
-    main.classList.remove('blur');
-}
-
-btnShowForm.addEventListener('click', () => {
-    openForm();
-    setTimeout(entryTitle.focus(), 400)
-})
-
-btnCloseForm.addEventListener('click', () => {
-    closeForm();
-})
-
-// Check validity of fields
-document.onready = function () {
-    document.querySelectorAll("input[required]").forEach(function (e) {
-       e.required = true;
-    });
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyADPTipmZZwOlPv4K3BxsAaWKNLBETeGvw",
+    authDomain: "library-app-8a5e5.firebaseapp.com",
+    databaseURL: "https://library-app-8a5e5.firebaseio.com",
+    projectId: "library-app-8a5e5",
+    storageBucket: "",
+    messagingSenderId: "328450636923",
+    appId: "1:328450636923:web:f1e51c2b44014e22"
   };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+
+// Shows entry form
+btnShowForm.addEventListener('click', () => {
+    newBookForm.style.visibility = 'visible';
+    setTimeout(titleBook.focus(), 400)
+})
+
+// Closes entry form
+btnCloseForm.addEventListener('click', () => {
+    newBookForm.style.visibility = 'hidden';
+})
